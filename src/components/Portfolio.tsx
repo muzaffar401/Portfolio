678bb3c5a3e9c5e544667128523863a433f7c90a
muzaffar401 <ma9400667@gmail.com>
2024-10-11 03:00:52 +0500

    PORTFOLIO

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,43 +1,52 @@
 "use client"
 import { motion } from 'framer-motion'
-import project1 from "../assets/proj1.jpg"
-import project2 from "../assets/proj2.jpg"
-import project3 from "../assets/proj3.png"
-import project4 from "../assets/proj4.jpg"
+import project1 from "../assets/p1.png"
+import project2 from "../assets/p2.png"
+import project3 from "../assets/p3.png"
+import project4 from "../assets/p4.png"
+import project5 from "../assets/p5.png"
 import Image from 'next/image'
 
 const projects = [
     {
-        title: "Ecommerce Digital Products",
-        desc: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est consequatur architecto distinctio laboriosam, ipsum officiis necessitatibus dignissimos nobis exercitationem, maxime voluptatibus deserunt iste tempore dolorum numquam tempora sequi saepe!",
+        title: "Food Leftover Website",
+        desc: "A food leftover website connects people or businesses with surplus food to those who need or want it, helping reduce food waste. Users can post or browse listings of excess food, promoting sustainability and addressing food insecurity by redistributing meals that would otherwise be wasted.",
         devStack: "HTML, CSS, JAVASCRIPT",
-        link: "#",
-        git: "#",
+        link: "https://muzaffar401.github.io/food.leftover.github.io/",
+        git: "https://github.com/muzaffar401/food.leftover.github.io.git",
         src: project1
     },
     {
-        title: "Ecommerce Digital Products",
-        desc: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est consequatur architecto distinctio laboriosam, ipsum officiis necessitatibus dignissimos nobis exercitationem, maxime voluptatibus deserunt iste tempore dolorum numquam tempora sequi saepe!",
+        title: "Metric Conversion Website",
+        desc: "A metric conversion website helps users convert between various units of measurement like length, weight, and volume. It provides tools for converting between metric and imperial systems for tasks such as cooking or engineering. The site offers a quick, accurate, and convenient way to handle everyday conversions.",
         devStack: "HTML, CSS, JAVASCRIPT",
-        link: "#",
-        git: "#",
+        link: "https://muzaffar401.github.io/metric_conversion.github.io/",
+        git: "https://github.com/muzaffar401/metric_conversion.github.io.git",
         src: project2
     },
     {
-        title: "Ecommerce Digital Products",
-        desc: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est consequatur architecto distinctio laboriosam, ipsum officiis necessitatibus dignissimos nobis exercitationem, maxime voluptatibus deserunt iste tempore dolorum numquam tempora sequi saepe!",
+        title: "Lifelink Website",
+        desc: "A LifeLink website connects individuals with essential health and emergency services. It provides tools for tracking medical information, accessing real-time health data, and receiving emergency alerts. Designed for quick communication, it ensures that users can easily contact healthcare providers or emergency responders, enhancing personal safety and well-being.",
         devStack: "HTML, CSS, JAVASCRIPT",
-        link: "#",
-        git: "#",
+        link: "https://lifelink-mu.vercel.app/",
+        git: "https://github.com/muzaffar401/Lifelink.github.io.git",
         src: project3
     },
     {
-        title: "Ecommerce Digital Products",
-        desc: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est consequatur architecto distinctio laboriosam, ipsum officiis necessitatibus dignissimos nobis exercitationem, maxime voluptatibus deserunt iste tempore dolorum numquam tempora sequi saepe!",
+        title: "Soccer Club Website",
+        desc: "A soccer club website provides fans and members with information about the club's teams, players, schedules, and events. It features updates on match results, news, and player stats, along with ticket sales and membership options. The site serves as a hub for fans to stay connected with their favorite club and engage with the soccer community",
         devStack: "HTML, CSS, JAVASCRIPT",
-        link: "#",
-        git: "#",
+        link: "https://muzaffar401.github.io/soccerverse.github.io/",
+        git: "https://github.com/muzaffar401/soccerverse.github.io.git",
         src: project4
+    },
+    {
+        title: "Space Portfolio Website",
+        desc: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est consequatur architecto distinctio laboriosam, ipsum officiis necessitatibus dignissimos nobis exercitationem, maxime voluptatibus deserunt iste tempore dolorum numquam tempora sequi saepe!",
+        devStack: "NEXT JS, TAILWIND CSS",
+        link: "https://space-portfolio-gilt-beta.vercel.app/",
+        git: "https://github.com/muzaffar401/space-portfolio.git",
+        src: project5
     },
 ]
 
@@ -63,7 +72,7 @@ const Portfolio = () => {
                         <div className='space-y-2 max-w-[550px] '>
                             <h2 className='text-7xl my-4 text-white/70'>{`0${index + 1}`}</h2>
                             <h2 className='text-4xl'>{project.title}</h2>
-                            <p className='text-lg text-white/70 break-words p-4'>{project.desc}</p>
+                            <p className='text-lg text-white/70 break-words p-2'>{project.desc}</p>
                             <p className='text-xl text-orange-400 font-semibold'>{project.devStack}</p>
 
                             <div className='w-64 h-[1px] bg-gray-400 my-4'>
@@ -76,7 +85,7 @@ const Portfolio = () => {
                             <Image
                                 src={project.src}
                                 alt={project.title}
-                                className='h-[350px] w-auto object-cover border border-gray-700 rounded'
+                                className='h-auto  w-auto object-cover border border-gray-700 rounded'
                             />
                         </div>
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -16,26 +16,29 @@ const skillIcons = [
     { icon: <SiTailwindcss size={140} />, label: "Tailwind CSS" },
     {
         icon: (
-            <Image 
-                src={nextjsIcon} 
-                alt="Next.js" 
+            <Image
+                src={nextjsIcon}
+                alt="Next.js"
                 className="w-[110px] h-[110px] lg:w-[140px] lg:h-[140px]"
             />
         ),
         label: "Next.js"
     }
-    
+
 ];
 
 const Skills = () => {
     return (
         <div className='bg-[linear-gradient(to_top,#000,#381a5f_80%)] py-32 overflow-x-hidden'>
-            <div className='text-white w-[400px] md:min-w-[950px] mx-auto p-8 text-center'>
+            <div className='text-white w-full md:max-w-[950px] mx-auto px-4 md:px-0 p-8 text-center'>
                 <h2 className='text-6xl font-bold mb-4'>What I Do</h2>
 
-                <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+                <div className='grid grid-cols-2 gap-6 justify-center md:grid-cols-4'>
                     {skillIcons.map((skill, index) => (
-                        <div key={index} className='h-[160px] w-[160px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl'>
+                        <div
+                            key={index}
+                            className='h-[160px] w-[160px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl'
+                        >
                             {skill.icon}
                             <p className='mt-2'>{skill.label}</p>
                         </div>
@@ -43,6 +46,7 @@ const Skills = () => {
                 </div>
             </div>
         </div>
+
     );
 };
 
